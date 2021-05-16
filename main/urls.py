47a62337ab8524473bda60b386a9a64055cee626
Faisal <faysalali534@gmail.com>
2021-05-16 16:25:40 +0500

    currencies endpoint added

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('input/', views.InputData.as_view(), name='input'),
     path('input/update/<int:id>', views.handle_input_update, name='input_update'),
     path('generate/queue', views.handle_queue_data, name='record_queue_data'),
+    path('currency/', views.Currency.as_view(), name='currencies'),
 
 ]

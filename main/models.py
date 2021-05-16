a675d02879f03f2a7503154b901c0f056668c8c9
uzair11 <wajahatahmad056@gmail.com>
2021-05-16 08:56:16 +0500

    added models

--- main/models.py
+++ main/models.py
@@ -20,15 +20,13 @@ class Account(models.Model):
     API credentials of user's account
     """
 
-
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_name')
     api_keys = models.CharField(max_length=250)
     secret_key = models.CharField(max_length=250)
 
-
     def get_account_client(self):
         return get_client(
-            exchange_id=self.exchange, api_key=self.api_key, secret=self.secret
+            api_key=self.api_key, secret=self.secret
         )
 
 
@@ -41,9 +39,7 @@ class Currency(models.Model):
 
 
 class Input(models.Model):
-    api_key = models.CharField(max_length=250)
-    secret = models.CharField(max_length=250)
-    api_key = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='api_key')
-    secret = models.ForeignKey(Account, on_delete= models.CASCADE,related_name='secret')
-
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='api_key')
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(auto_now_add=True)
 

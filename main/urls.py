8e38e0d404954a89e32d7d2b20ac1c4ea45d1e9a
uzair11 <wajahatahmad056@gmail.com>
2021-05-16 11:51:17 +0500

    developed basic structure to queue

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from main import views
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
     path('input/', views.InputData.as_view(), name='input'),
-    path('input/update/<int:id>', views.handle_input_update, name='input_update')
+    path('input/update/<int:id>', views.handle_input_update, name='input_update'),
+    path('generate/queue', views.handle_queue_data, name='record_queue_data'),
 
 ]

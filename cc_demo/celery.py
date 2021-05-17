cbffa012ccc1d3b2aea86340cdef1759276c6b69
uzair11 <wajahatahmad056@gmail.com>
2021-05-17 18:43:52 +0500

    Queue implemented

--- cc_demo/celery.py
+++ cc_demo/celery.py
@@ -1,8 +1,10 @@
 import os
 
 from celery import Celery
-
+from django.apps import apps
 # set the default Django settings module for the 'celery' program.
+from cc_demo import settings
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cc_demo.settings')
 
 app = Celery('cc_demo')
@@ -14,8 +16,8 @@ app = Celery('cc_demo')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
-
+app.config_from_object(settings)
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
 @app.task(bind=True)
 def debug_task(self):
--- main/tasks.py
+++ main/tasks.py
@@ -2,8 +2,23 @@ from celery import shared_task
 
 from time import sleep
 
+from main.api import ExchangeManipulation
+from main.models import Input
+
 
 @shared_task(bind=True)
-def go_to_sleep(self, duration):
-    sleep(duration)
-    return 'Done'
+def generate_balance_and_leger(self, pk_num):
+    sleep(15)
+    input_instance = Input.objects.get(pk=pk_num)
+    exchange_manipulation = ExchangeManipulation(
+        start_date=input_instance.start_date,
+        end_date=input_instance.end_date,
+        currency=input_instance.currency.short_code,
+        category=input_instance.category,
+        exchange_id="bybit",
+        api_key=input_instance.account.api_key,
+        secret_key=input_instance.account.secret_key,
+        input_instance=input_instance,
+    )
+    exchange_manipulation.generate_balance_and_leger()
+    return "ok"

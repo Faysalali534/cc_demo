984b8ec1e5adbc9c9cbb251de40f60b9e167f07e
Faisal <faysalali534@gmail.com>
2021-05-17 19:16:26 +0500

    included account_id on login

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
 from main import views
+from main.views import CustomAuthToken
 
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
@@ -9,7 +10,7 @@ urlpatterns = [
     path('input/update/<int:id>', views.handle_input_update, name='input_update'),
     path('generate/queue', views.handle_queue_data, name='record_queue_data'),
     path('currency/', views.Currency.as_view(), name='currencies'),
-    path('login/', obtain_auth_token, name='login'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
     path('check/', views.CheckAuth.as_view(), name='test'),
     path('logout/', views.Logout.as_view(), name='logout'),
 

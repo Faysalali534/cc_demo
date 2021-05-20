6d2f8132939306fe7974d29a89acb6d0ba361c37
Faisal <faysalali534@gmail.com>
2021-05-20 13:12:18 +0500

    logs endpoint added

--- main/views.py
+++ main/views.py
@@ -163,3 +163,15 @@ class LogsData(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Log.objects.all()
     serializer_class = LogsSerializer
+    lookup_field = 'input_id'
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset()).filter(input_id=kwargs.get('input_id'))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
